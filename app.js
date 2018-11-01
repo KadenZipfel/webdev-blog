b31aa3709eb67f1f5afdb76817cba22caa42a811
unknown <kaden.zipfel@hotmail.com>
2018-11-01 15:47:44 -0700

    Setup database

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const keys = require('./keys');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// Setup database
+mongoose.connect('mongodb://localhost/webdev-blog') || mongoose.connect(keys.db);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error: '));
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
